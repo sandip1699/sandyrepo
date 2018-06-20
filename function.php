5744e61afc597333979713b1177c8dc6ec7f0b80
sandip1699 <sandippatel1699@gmail.com>
2018-06-20 11:47:57 +0530

    Create function.php

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * theme functions and definitions
+ *
+
+// Before VC Init
+add_action( 'vc_before_init', 'vc_before_init_actions' );
+ 
+function vc_before_init_actions() {
+     
+    // Require new custom Element
+    require_once( get_template_directory().'/vc-elements/vc_shortcodes.php' ); 
+     
+}
